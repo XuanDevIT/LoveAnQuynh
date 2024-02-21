f690e11b20984f287c13873bba0c39a8d26cd24f
Xuân Lê <levanxuan5518@gmail.com>
2024-02-22 00:15:07 +0700

    first commit

---
+++ CONFIG.js
@@ -0,0 +1,10 @@
+const CONFIGDATA = {
+  titleHeader: "Chào Quỳnh, Anh có điều muốn nói. </br> Làm người yêu anh nhé!",
+  descriptionHeader:
+    "Hãy trả lời một cách thật lòng nhé. Nếu em im lặng và rời đi thì tức là em đã ĐỒNG Ý.",
+  buttonYes: "Em đồng ý",
+  buttonNo: "Không đồng ý",
+  titleModar: "Cảm ơn Em.",
+  descriptionModar:
+    "Cảm ơn Em đã cho tớ một cơ hội để anh có thể ở bên cạnh, che chở và bảo vệ em. </br> Anh biết Anh không được đẹp trai, cũng chẳng lắm tài nhưng anh có một trái tim chân thành. Em chính là cả thế giới của Anh. </br> Tối nay Anh đã là người hạnh phúc nhất trần gian. </br> Love you <3",
+};
